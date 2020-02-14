d526ad2cabd9f97cb51ef6516a29f17aac896c96
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-14 10:28:33 +0000

    Created first failed test

--- Chessington.GameEngine.Tests/Pieces/KingTests.cs
+++ Chessington.GameEngine.Tests/Pieces/KingTests.cs
@@ -1,9 +1,22 @@
-﻿﻿using NUnit.Framework;
+﻿﻿using Chessington.GameEngine.Pieces;
+ using FluentAssertions;
+ using NUnit.Framework;
 
 namespace Chessington.GameEngine.Tests.Pieces
 {
     [TestFixture]
     public class KingTests
     {
+        [Test]
+        public void WhiteKing_CanMoveOneSquareUp()
+        {
+            var board = new Board();
+            var king = new King(Player.White);
+            board.AddPiece(Square.At(4, 0), king);
+
+            var moves = king.GetAvailableMoves(board);
+
+            moves.Should().Contain(Square.At(3, 0));
+        }
     }
 }
