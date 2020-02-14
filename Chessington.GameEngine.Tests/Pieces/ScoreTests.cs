7909f8bea77ad8288b8a90bf9127f1cf738be910
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-14 16:45:15 +0000

    Score balance built failing test

--- Chessington.GameEngine.Tests/Pieces/ScoreTests.cs
+++ Chessington.GameEngine.Tests/Pieces/ScoreTests.cs
@@ -61,6 +61,28 @@ namespace Chessington.GameEngine.Tests.Pieces
             var scoreCalculator = new ScoreCalculator(board);
             scoreCalculator.WhoIsWinning().Should().Be("Black is in the lead!");
         }
+        
+        [Test]
+        public void TestScoreBalance()
+        {
+            //Black 18
+            var rook = new Rook(Player.Black); //5
+            var queen = new Queen(Player.Black); //9
+            var pawn2 = new Pawn(Player.Black); //1
+            var bishop = new Bishop(Player.Black); //3
+            
+            //White 13
+            var knight = new Knight(Player.White); //3
+            var queen2 = new Queen(Player.White); //9
+            var pawn3 = new Pawn(Player.White); //1
+            
+            var board = A.Fake<IBoard>();
+            A.CallTo(() => board.CapturedPieces).Returns(new List<Piece> {rook,queen,knight,queen2,pawn2,pawn3,bishop});
+            
+            
+            var scoreCalculator = new ScoreCalculator(board);
+            scoreCalculator.WhoIsWinning().Should().Be("Black has taken a slight lead!");
+        }
     }
 }
  
