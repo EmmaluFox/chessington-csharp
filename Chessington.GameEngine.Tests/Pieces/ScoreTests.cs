9eccfea51608eec8230c4714aba3bd1577dcee8c
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-14 14:32:40 +0000

    ScoreCalculator - created failing test

---
+++ Chessington.GameEngine.Tests/Pieces/ScoreTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Chessington.GameEngine.Tests.Pieces
+{
+    [TestFixture]
+    public class ScoreTests
+    {
+        [Test]
+        public void TestScoreCalculator()
+        {
+            var rook = new Rook(Player.White);
+            var board = A.Fake<IBoard>();
+            A.CallTo(() => board.CapturedPieces).Returns(new List<Piece> {rook});
+            var scoreCalculator = new ScoreCalculator(board);
+
+
+            scoreCalculator.GetWhiteScore().Should().Be(5);
+        }
+    }
+}
