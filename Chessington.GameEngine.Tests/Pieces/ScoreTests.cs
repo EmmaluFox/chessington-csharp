2737d7bea338b484ccbc97b76785752b5f97af27
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-14 14:55:49 +0000

    Failing test 2

--- Chessington.GameEngine.Tests/Pieces/ScoreTests.cs
+++ Chessington.GameEngine.Tests/Pieces/ScoreTests.cs
@@ -19,6 +19,27 @@ namespace Chessington.GameEngine.Tests.Pieces
 
             scoreCalculator.GetWhiteScore().Should().Be(5);
         }
+        
+        
+        [Test]
+        public void AnotherTestScoreCalculator()
+        {
+            var rook = new Rook(Player.Black);
+            var queen = new Queen(Player.Black);
+            var pawn2 = new Pawn(Player.Black);
+            
+            
+            var knight = new Knight(Player.White);
+            var pawn = new Pawn(Player.White);
+            var pawn3 = new Pawn(Player.White);
+            
+            var board = A.Fake<IBoard>();
+            A.CallTo(() => board.CapturedPieces).Returns(new List<Piece> {rook,queen,knight,pawn,pawn2,pawn3});
+            var scoreCalculator = new ScoreCalculator(board);
+
+            scoreCalculator.GetWhiteScore().Should().Be(5);
+            scoreCalculator.GetBlackScore().Should().Be(15);
+        }
     }
 }
 
